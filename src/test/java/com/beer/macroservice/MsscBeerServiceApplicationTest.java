2cb09e180a8e0b01953d9968ae0730fff1ffbd01
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2019-12-06 16:12:24 +0200

    setup junit5 test framework

--- src/test/java/com/beer/macroservice/MsscBeerServiceApplicationTest.java
+++ src/test/java/com/beer/macroservice/MsscBeerServiceApplicationTest.java
@@ -1,12 +1,10 @@
 package com.beer.macroservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.beer.microservice.MsscBeerServiceApplication;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = MsscBeerServiceApplication.class)
 public class MsscBeerServiceApplicationTest {
 
     @Test

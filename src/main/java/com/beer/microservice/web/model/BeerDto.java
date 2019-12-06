575c58c9eb966f6713b937bd2f1c519d1f1c6763
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2019-12-06 15:25:34 +0200

    add domain model

---
+++ src/main/java/com/beer/microservice/web/model/BeerDto.java
@@ -0,0 +1,26 @@
+package com.beer.microservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/com/beer/microservice/web/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package com.beer.microservice.web.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
